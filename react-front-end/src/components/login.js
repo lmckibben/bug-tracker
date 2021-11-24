03b61c54770f32a30b08d5d69e7b9d4db9e926de
Leland Mckibben <lmckibben28@hotmail.com>
2021-11-24 05:21:57 +0000

    created login page

--- react-front-end/src/components/login.js
+++ react-front-end/src/components/login.js
@@ -1,18 +1,63 @@
 import React from "react";
-import { Grid, Paper } from "@material-ui/core"
+import { Avatar, Grid, Paper, TextField, Typography, Link } from "@material-ui/core";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { FormControlLabel } from "@material-ui/core";
+import { Checkbox } from "@material-ui/core";
+import Button from "@mui/material/Button";
 
 const Login = () => {
 
   const paperStyle = {
     padding: 20,
-    height: '70vh',
-    width: 500,
-    margin: '20px auto'
+    height: '50vh',
+    width: 400,
+    margin: '200px auto'
   }
+
+  const avatarStyle = {
+    backgroundColor: '#3F72AF'
+  }
+
+  const buttonStyle = {
+    margin: '15px 0'
+  }
+
   return (
     <Grid>
       <Paper elevation={10} style={paperStyle}>
-        Sign In
+        <Grid align="center">
+          <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
+          <h2>Sign In</h2>
+        </Grid>
+        <TextField label="email" placeholder="email" fullWidth required/>
+        <TextField label="password" placeholder="password" type="password" fullWidth required/>
+        <FormControlLabel
+        control={
+          <Checkbox
+            name="checkedB"
+            color="primary"
+          />
+        }
+        label="Remember Me"
+      />
+      <Button type="submit" color="primary" variant="contained" style={buttonStyle} fullWidth>Sign In</Button>
+      <br></br>
+      <br></br>
+      <Typography> Forgot your {' '}
+        <Link href="#">
+          password?
+        </Link>
+      </Typography>
+      <Typography> Don't have an account? {' '}
+        <Link href="#">
+          Register
+        </Link>
+      </Typography>
+      <Typography> Sign in as a {' '}
+        <Link href="#">
+          Demo User
+        </Link>
+      </Typography>
       </Paper>
     </Grid>
   );
